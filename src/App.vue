5f083f9ec0a16449883e31f44b729ec851e2d6dd
yansiqi <1749787628@qq.com>
2020-05-05 00:59:47 +0800

    finish

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
+    <Header></Header>
     <router-view></router-view>
+    <!-- 1种方式 -->
+    <!-- <Footer v-show="true"></Footer> -->
+    <!-- 2种方式 -->
+    <!-- 当不是注册和登录的时候才会显示 -->
+    <!-- <Footer v-show="$route.path!=='/register'&& $route.path!=='/login' "></Footer> -->
+     <!-- 3种方式 !的时候才会显示啊 -->
+<Footer v-show="!$route.meta.isHideFooter"></Footer>
   </div>
 </template>
 
 <script>
+import Header from './components/Header';
+import Footer from './components/Footer'
 export default {
 name:'App',
+components: {
+  Header,
+  Footer
+}
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,13 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-//引入组件 主页/搜索页面/注册页面/登录页面
-import Home from "@/pages/Home";
-import Search from "@/pages/Search";
-import Register from "@/pages/Register";
-import Login from "@/pages/Login";
+import routes from './routes'
 
 //声明使用vue插件
 Vue.use(VueRouter);
 //暴露一个路由器对象
 export default new VueRouter({
+  //处理#
+  mode:'history',//不带#
   /* 配置所有路由--->使用数组 */
-  routes: [
-      /* 注意路由路径名小写.路由组件名大写 */
-    {
-      path: "/",
-      component: Home,
-    },
-    {
-        path: "/search",
-        component: Search,   
-    },
-    {
-        path: "/register",
-        component: Register,   
-    },
-    {
-        path: "/login",
-        component: Login,   
-    },
-  ],
+  routes
 });
