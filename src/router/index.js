53695f756cc23aa56a4f5fcca122c311ee69349d
yansiqi <1749787628@qq.com>
2020-05-05 07:48:00 +0800

    修改

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,36 @@ import routes from './routes'
 
 //声明使用vue插件
 Vue.use(VueRouter);
+/* 
+解决2: 修正Vue原型上的push和replace方法 (优秀)
+*/
+// 缓存原型上的push方法
+const originPush = VueRouter.prototype.push
+const originReplace = VueRouter.prototype.replace
+// 重新指定原型上的push方法
+VueRouter.prototype.push = function (location, onComplete, onAbort) {
+  console.log('push()', location, onComplete, onAbort)
+  // this是路由器对象 $router
+  // 如果调用push, 传递了成功或者失败的回调函数
+  if (onComplete || onAbort) {
+    // 让原来的push方法进行处理
+    originPush.call(this, location, onComplete, onAbort) // 不用返回, 因为执行的结果返回是undfined
+  } else { // 如果调用push, 没传递了成功或者失败的回调函数, 可能会抛出失败的promise, 需要catch一下
+    return originPush.call(this, location).catch(() => {
+      console.log('catch error')
+    })   // 必须返回产生的promise对象
+  }
+}
+
+VueRouter.prototype.replace = function (location, onComplete, onAbort) {
+  if (onComplete || onAbort) {
+    originReplace.call(this, location, onComplete, onAbort) 
+  } else {
+    return originReplace.call(this, location).catch(() => {
+      console.log('catch error2')
+    })   
+  }
+}
 //暴露一个路由器对象
 export default new VueRouter({
   //处理#
