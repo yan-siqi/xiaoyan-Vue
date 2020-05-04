d4f04ccdf271ca537d5bb1b9c8ce30cb2fe72f5c
yansiqi <1749787628@qq.com>
2020-05-04 21:54:55 +0800

    l路由配置

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
+/* import App from './App.vue' */
+import App from '@/App.vue'//没有任何提示,在没有配置之前
+import router from './router'
+Vue.config.productionTip = false//去掉不是生产环境的模式
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App),//将组件的元素,渲染到页面上
+  router//配置路由器
 }).$mount('#app')
+
---
+++ vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    lintOnSave: false, // 关闭eslint规则检查
+  }
